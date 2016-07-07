b4eaf41a17983ff7771671fb83ab77cb76cb7f85
iwatobipen <seritaka@gmail.com>
2016-07-08 06:32:38 +0900

    use tsne

---
+++ chemicalspace2/chemmap.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import DataStructs
+from sklearn.decomposition import PCA
+import pickle, sys
+
+drugs = [ mol for mol in Chem.SDMolSupplier( "st178.sdf" ) if mol != None ]
+
+def calc_fp_arr( mols ):
+    fplist = []
+    for mol in mols:
+        arr = np.zeros( (1,) )
+        fp = AllChem.GetMorganFingerprintAsBitVect( mol, 2 )
+        DataStructs.ConvertToNumpyArray( fp, arr )
+        fplist.append( arr )
+    return np.asarray( fplist )
+
+res=calc_fp_arr(drugs)
+pca = PCA( n_components = 3 )
+pca.fit( res )
+f = open( 'pca.pkl', 'wb' )
+pickle.dump( pca, f )
+f.close()
+
+
+x = pca.transform( res )
+f = open( "pcares.txt", "w" )
+
+for i in range( x.shape[0] ):
+    line = ""
+    line = Chem.MolToSmiles( drugs[i] ) + "," + str( x[i][0] )  + "," + str( x[i][1] ) + "\n"
+    f.write( line )
+f.close() 
